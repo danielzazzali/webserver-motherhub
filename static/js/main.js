3b147569ca99daf11914e7536521c987d41647c7
Daniel Zazzali <dazazzali@gmail.com>
2024-12-06 12:34:47 -0300

    added loading modal to shutdown and reboot

--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,27 @@
 async function shutdownAsync() {
     showConfirmation('Are you sure you want to shut down the system?', async () => {
-        const response = await shutdownSystem();
-        if (response.error) {
-            console.error('Error:', response.error);
-        } else {
-            console.log(response.message);
-        }
+        closeModal(document.getElementById('confirmation-modal'));
+        showLoadingModal();
+        shutdownSystem().then(response => {
+            if (response.error) {
+                console.error('Error:', response.error);
+            } else {
+                console.log(response.message);
+            }
+        });
     });
 }
 
 async function rebootAsync() {
     showConfirmation('Are you sure you want to reboot the system?', async () => {
-        const response = await rebootSystem();
-        if (response.error) {
-            console.error('Error:', response.error);
-        } else {
-            console.log(response.message);
-        }
+        closeModal(document.getElementById('confirmation-modal'));
+        showLoadingModal();
+        rebootSystem().then(response => {
+            if (response.error) {
+                console.error('Error:', response.error);
+            } else {
+                console.log(response.message);
+            }
+        });
     });
 }
-
-
