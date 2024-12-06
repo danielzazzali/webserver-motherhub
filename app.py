80ee4156a68f1af0c6878066bd7c548fcf29aafc
Daniel Zazzali <dazazzali@gmail.com>
2024-12-06 13:54:01 -0300

    Deleted trash code

--- app.py
+++ app.py
@@ -15,37 +15,5 @@ app.register_blueprint(wifi_bp)
 def index():
     return render_template('index.html')
 
-def test_api():
-    with app.test_client() as client:
-
-        #############################################################################
-        ###################### Test the mode API  ###################################
-        #############################################################################
-        # Test the device mode API
-        device_mode = client.get('/device_mode')
-        print(device_mode.get_json())
-
-        device_mode = client.post('/device_mode', json={'mode': 'NOTAPNORSTA'})
-        print(device_mode.get_json())
-
-        #device_mode = client.post('/device_mode', json={'mode': 'AP'})
-        #print(device_mode.get_json())
-
-        #############################################################################
-        ###################### Test the ethernet API  ###############################
-        #############################################################################
-
-        devices = client.get('/connected_devices')
-        print(devices.get_json())
-
-
-        #############################################################################
-        ###################### Test the wi-fi API  ##################################
-        #############################################################################
-
-
-
-
 if __name__ == '__main__':
-    #test_api()
     app.run(port=port)
